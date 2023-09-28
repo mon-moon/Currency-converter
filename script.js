893eb52999e857550472bf1073cbb23aa887fab1
Monika Sawicka <monik.sawicka@gmail.com>
2023-09-28 15:53:27 +0200

    update to function

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-const writeamount = document.querySelector(".js-amount");
-const formelement = document.querySelector(".js-form");
-const exchangerate = document.querySelector(".js-selectRate"); 
-const yourcurrencyElement = document.querySelector(".js-yourcurrency");
-const resetElement = document.querySelector(".js-reset");
+{
+  const writeamount = document.querySelector(".js-amount");
+  const formelement = document.querySelector(".js-form");
+  const exchangerate = document.querySelector(".js-selectRate");
+  const yourcurrencyElement = document.querySelector(".js-yourcurrency");
+  const resetElement = document.querySelector(".js-reset");
 
-const USD = 4.40;
-const EUR = 4.60;
-const GBP = 5.00;
-
-formelement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const amount = parseFloat(writeamount.value); 
-    const currency = exchangerate.value;
-    let result;
-
+  const calculateResult = (amount, currency) => {
+    const USD = 4.4;
+    const EUR = 4.6;
+    const GBP = 5.0;
     switch (currency) {
-        case "USD":
-            result = amount / USD;
-            break; 
-
-        case "EUR":
-            result = amount / EUR;
-            break; 
-        case "GBP":
-            result = amount / GBP;
-            break; 
+      case "USD":
+        return amount / USD;
+      case "EUR":
+        return amount / EUR;
+      case "GBP":
+        return amount / GBP;
     }
+  };
 
-    yourcurrencyElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
-
-resetElement.addEventListener("click", () => {
-    writeamount.value = ""; 
-    yourcurrencyElement.innerHTML = "N/a"; 
-});
+ const init = () => {
+    formelement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const amount = +writeamount.value;
+        const currency = exchangerate.value;
+        const result = calculateResult(amount, currency);
+    
+        yourcurrencyElement.innerHTML = `${amount.toFixed(
+          2
+        )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+      });
+    
+      resetElement.addEventListener("click", () => {
+        writeamount.value = "";
+        yourcurrencyElement.innerHTML = "N/a";
+      });
+ };
+ init();
+}
